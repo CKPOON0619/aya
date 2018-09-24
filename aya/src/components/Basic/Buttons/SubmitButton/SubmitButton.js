a80746eb7ddc7218eaa96611c3fef6cf2ea96b5c
kin <kin.poon@concentra.co.uk>
2018-09-24 12:11:14 +0100

    Changes to buttons

--- aya/src/components/Basic/Buttons/SubmitButton/SubmitButton.js
+++ aya/src/components/Basic/Buttons/SubmitButton/SubmitButton.js
@@ -1,8 +1,21 @@
-import React from "react";
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
 import "./SubmitButton.css";
+const styles = theme => ({
+  button: {
+    margin: theme.spacing.unit,
+  },
+  input: {
+    display: 'none',
+  },
+});
 
 const button = props => (
-  <button className="SubmitButton-submit" onClick={props.clicked}>{props.label}</button>
+  <div className="SubmitButton-submit">
+    <Button variant="contained" color="primary" onClick={props.clicked}>{props.label}</Button>
+  </div>
 );
 
-export default button;
+export default withStyles(styles)(button);
